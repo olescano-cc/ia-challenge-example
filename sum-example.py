1b7c70cc862ea2c20e9d5401c1192dbd8ad34304
olescano-cc <olescano@cleancompany.com>
2025-01-15 16:05:24 -0300

    changes

--- sum-example.py
+++ sum-example.py
@@ -1,6 +1,8 @@
 def sum(num1, num2):
-    num3 = num2
-    num5 = num1
-    return num5 + num3
+    return num1 + num2
+
+def substract(num1, num2):
+    return num1, num2
 
 print(sum(1, 2))
+print(substract(1, 2))
